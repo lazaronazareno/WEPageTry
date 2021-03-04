9bcbf9e29af530f0a764536d0c8d4e9c14abe23b
lazaronazareno <57498171+lazaronazareno@users.noreply.github.com>
2021-03-04 00:36:40 -0300

    section "nosotros" finished, also half of "fotos", new styles for the sidebarpages and upload a new logo "nombrelogo.png". Marginless body and width - height defined for App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./styles.css";
 //import Frontpage from "./components/frontpage/frontpage";
-import Home from "./components/home/home";
-import Sidebar from "./components/sidebar/sidebar";
+//import Home from "./components/home/home";
+//import Sidebar from "./components/sidebar/sidebar";
+//import Contactos from "./components/sidepages/Contactos/contactos";
+//import Nosotros from "./components/sidepages/Nosotros/nosotros";
+import Fotos from "./components/sidepages/Fotos/fotos";
 
 function App() {
   return (
@@ -12,7 +15,7 @@ function App() {
       {
         //<Home />
       }
-      <Sidebar />
+      <Fotos />
     </div>
   );
 }
