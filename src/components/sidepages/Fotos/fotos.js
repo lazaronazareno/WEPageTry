10b71792abf535db43eb5da83eb85116f0a17a72
lazaronazareno <57498171+lazaronazareno@users.noreply.github.com>
2021-03-05 00:44:38 -0300

    section Fotos finished, add a carousel reusable component with props(images)

--- src/components/sidepages/Fotos/fotos.js
+++ src/components/sidepages/Fotos/fotos.js
@@ -1,5 +1,7 @@
+import Carousel from "../../carousel/carousel";
 import Sidebar from "../../sidebar/sidebar";
 import "./styles.scss";
+import photos from "../../../assets/photos";
 
 function Fotos() {
   return (
@@ -7,14 +9,15 @@ function Fotos() {
       <Sidebar />
       <div className="photosMain">
         <h1>FOTOS</h1>
-        <h3>
+        <h2 className="subtitulo">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        </h2>
+        <Carousel images={photos} />
+        <h3 className="text">
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
+          aliquip ex ea commodo consequat."
         </h3>
       </div>
     </div>
